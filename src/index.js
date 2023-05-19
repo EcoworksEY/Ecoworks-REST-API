56f7c9a8af2879f4b4d43077a31dbb5ed219ab06
bradkhoda <brad.khoda@au.ey.com>
2023-05-19 13:25:45 +1000

    Fixed errors with SQL

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const product_router = require("./routers/product_router")
 
 // App setup with required dependencies
 const app = express();
@@ -13,8 +15,9 @@ const port = process.env.PORT || 3000;
 app.use(helmet());
 app.use(bodyParser.json());
 
+
 // Configure the app to use the routers
-app.use("/product", require("./product_router.js"));
+app.use("/product", product_router);
 
 // Start the app
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
--- src/middleware/database_connector.js
+++ src/middleware/database_connector.js
@@ -2,17 +2,15 @@
 // Written by Bardia Habib Khoda
 
 // Library Injections
-const { Pool } = require('pg');
+const { Client } = require('pg');
 
-const database_connector = new Pool({
+const database_connector = new Client({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    ssl: {
-        rejectUnauthorized: false // For self-signed certificates, set this to false
-    }
+    port: process.env.DB_PORT
 });
 
-module.export = database_connector;
+database_connector.connect();
+
+module.exports = database_connector;
