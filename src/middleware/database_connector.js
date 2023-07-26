36c62a163a0482ea52aeadf3d0f2f628fddea4f0
bradkhoda <125939293+bradkhoda@users.noreply.github.com>
2023-07-26 15:29:29 +1000

    Update database_connector.js
    Signed-off-by: bradkhoda <125939293+bradkhoda@users.noreply.github.com>

--- src/middleware/database_connector.js
+++ src/middleware/database_connector.js
@@ -2,13 +2,15 @@
 // Written by Bardia Habib Khoda
 // Library Injections
 const { Client } = require('pg');
+console.log(' We got in here.');
+console.log(`host is: ${process.env.DB_HOST}`);
+
 const database_connector = new Client({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10)
 });
-
 
 database_connector.connect();
 
