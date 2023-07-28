09e3483bce79db0e338aa3bae2931e07c46f68be
bradkhoda <125939293+bradkhoda@users.noreply.github.com>
2023-07-28 15:19:11 +1000

    Added Health Check
    Signed-off-by: bradkhoda <125939293+bradkhoda@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const product_router = require("./routers/product_router");
 const user_router = require('./routers/user_router');
+const health_check_router = require('./routers/health_check_router');
 const cors = require('cors');
 
 // App setup with required dependencies
@@ -22,6 +23,7 @@ app.use(cors());
 // Configure the app to use the routers
 app.use("/product", product_router);
 app.use("/auth", user_router);
+app.use("/", health_check_router);
 
 // Start the app
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
