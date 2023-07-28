5280ae52dc25285d99e64294796b2bb7a9c31394
bradkhoda <125939293+bradkhoda@users.noreply.github.com>
2023-07-28 15:18:15 +1000

    Create health_check_router.js
    Signed-off-by: bradkhoda <125939293+bradkhoda@users.noreply.github.com>

---
+++ src/routers/health_check_router.js
@@ -0,0 +1,13 @@
+// Router for health checks
+// Written by Bardia Habib Khoda
+
+// Library injections
+const express = require("express");
+const health_check_router = express.Router();
+
+health_check_router.get('', async (req, res) => {
+res.status(200).json({ message: 'OK'});
+});
+
+// Export router
+module.exports = health_check_router;
